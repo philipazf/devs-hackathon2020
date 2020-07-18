85d9389893f43eaadecc70287e45a830582aedcc
Harpreet Singh <hsingh9600@gmail.com>
2020-07-18 16:39:23 +1200

    adding tasks

--- app.js
+++ app.js
@@ -6,23 +6,24 @@ const app = express();
 
 let nextId = 0;
 let currentId = 0;
+let nextSkillId = 0;
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.get("/signup", function(req, res) {
+app.get("/signup", function (req, res) {
     res.render("signup");
 
     let sqlQuery = "SELECT MAX(id) AS id FROM appdatabase.users";
 
-    db.query(sqlQuery, function(err, result) {
-        if(err) {
+    db.query(sqlQuery, function (err, result) {
+        if (err) {
             throw err;
         }
         else {
@@ -31,51 +32,90 @@ app.get("/signup", function(req, res) {
     });
 });
 
-app.post("/signup", function(req, res) {
+app.post("/signup", function (req, res) {
     let email = req.body.email;
     let name = req.body.name;
     let password = req.body.password;
 
-    let sql = "INSERT INTO appdatabase.users VALUES (" + nextId + ", \"" + name + "\", \"" + String(email) + "\", \"" + password + "\", 0)";
+    let sql = "INSERT INTO appdatabase.users VALUES (" + nextId + ", \"" + name + "\", \"" + String(email) + "\", \"" + password + "\")";
     let userTaskTable = "CREATE TABLE appdatabase.user" + nextId + " (id INT NOT NULL, name TEXT, description TEXT, PRIMARY KEY (id) )";
 
-    db.query(sql, function(err, result) {
-        if(err) {
+    db.query(sql, function (err, result) {
+        if (err) {
             throw err;
         }
         else {
-            db.query(userTaskTable, function(err, result) {
-                if(err) {
+            db.query(userTaskTable, function (err, result) {
+                if (err) {
                     throw err;
                 }
                 else {
                     currentId = nextId;
-                    res.redirect("/user/" + currentId);
+                    let sql = "SELECT * FROM appdatabase.users where id = " + currentId;
+                    db.query(sql, function (err, result) {
+                        if (err) {
+                            throw err;
+                        }
+                        else {
+                            let addVal = "INSERT INTO appdatabase.user" + currentId + " VALUES (" + 0 + ", \"temp\", \"temp\")";
+                            db.query(addVal, function (err, result) {
+                                if (err) {
+                                    throw err;
+                                }
+                            });
+                            res.redirect("/user/" + currentId);
+                        }
+                    });
                 }
             });
         }
     });
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/user/:id", function(req, res) {
-    let sql = "SELECT * FROM appdatabase.users where id = " + req.params.id;
-    db.query(sql, function(err, result) {
-        if(err) {
+app.get("/user/:id", function (req, res) {
+    if(req.params.id == currentId) {
+        res.render("profile");
+
+    let sqlQueryMax = "SELECT MAX(id) AS id FROM appdatabase.user" + currentId;
+
+    db.query(sqlQueryMax, function (err, result) {
+        if (err) {
             throw err;
         }
-        else if(req.params.id == currentId) {
-            res.render("profile", {userName: result[0].name, userPoints: result[0].points});
+        else {
+            nextSkillId = result[0].id + 1;
+            console.log(result[0].id);
+        }
+    });
+
+    }
+    else {
+        res.send("Login Error");
+    }
+});
+
+app.post("/addskill",function(req, res) {
+    let skillname = req.body.skillname;
+    let skilldescription = "lregfnekljgn fejgnerkjwlgn ewrskillgnerljk";
+
+    let sqlQuery = "INSERT INTO appdatabase.user" + currentId + " VALUES (" + nextSkillId + ", \"" + skillname + "\", \"" + skilldescription + "\")";
+
+    db.query(sqlQuery, function (err, result) {
+        if (err) {
+            throw err;
         }
         else {
-            res.send("Login Error");
+            console.log("fsgfdsgsdf");
+            console.log(result);
+            res.redirect("/user/" + currentId);
         }
     });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
